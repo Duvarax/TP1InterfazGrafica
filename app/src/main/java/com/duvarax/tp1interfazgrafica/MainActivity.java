b44b0563ba074b91caa6b44abedb476c6c643b31
Duvara Claudio <91039016+Duvarax@users.noreply.github.com>
2023-03-24 19:43:31 -0300

    Entrega

---
+++ app/src/main/java/com/duvarax/tp1interfazgrafica/MainActivity.java
@@ -0,0 +1,66 @@
+package com.duvarax.tp1interfazgrafica;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.duvarax.tp1interfazgrafica.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    MainActivityViewModel mv;
+
+    Context context;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        mv = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(MainActivityViewModel.class);
+        context = getApplicationContext();
+        mv.getValorConvertido().observe(this, new Observer<Double>() {
+            @Override
+            public void onChanged(Double conversion) {
+                binding.tvConversion.setText(conversion+"");
+            }
+        });
+
+
+        binding.btnConvertir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    if(binding.rbDolaresAEuros.isChecked()){
+                        mv.convertirDolarAEuro(Double.parseDouble(binding.etDolares.getText().toString()));
+                    }
+                    if(binding.rbEurosADolar.isChecked()){
+                        mv.convertirEuroADolar(Double.parseDouble(binding.etEuros.getText().toString()));
+                    }
+                }catch(NumberFormatException empetyString){
+                    Toast.makeText(context, "Debe ingresar la cantidad de la moneda para hacer la conversion", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        binding.rbDolaresAEuros.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.rbEurosADolar.setChecked(false);
+                binding.etEuros.setText("");
+            }
+        });
+        binding.rbEurosADolar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.rbDolaresAEuros.setChecked(false);
+                binding.etDolares.setText("");
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/duvarax/tp1interfazgrafica/MainActivityViewModel.java
@@ -0,0 +1,42 @@
+package com.duvarax.tp1interfazgrafica;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+public class MainActivityViewModel extends AndroidViewModel {
+
+    private Context context;
+    private MutableLiveData<Double> valorConvertido;
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        this.context = application.getApplicationContext();
+    }
+
+    public LiveData<Double> getValorConvertido(){
+        if (valorConvertido == null){
+            valorConvertido = new MutableLiveData<>();
+        }
+        return valorConvertido;
+    }
+
+    public void convertirDolarAEuro(Double dolar){
+        try{
+            valorConvertido.setValue(dolar*0.93);
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    public void convertirEuroADolar(Double euro){
+        try{
+            valorConvertido.setValue(euro*1.08);
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
